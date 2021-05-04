a47507f42eaa7b1e12c197170223d3288c2fcfe4
asit <asitgraphics@gmail.com>
2021-05-04 12:49:03 +0530

    update

--- assets/js/viewPassword.js
+++ assets/js/viewPassword.js
@@ -6,5 +6,5 @@ togglePassword.addEventListener('click', function (e) {
     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', type);
     // toggle the eye slash icon
-    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
 }); 
